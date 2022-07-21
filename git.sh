dbddc9a7b02defc3df62e62462005388b1c282fa
Ваше Имя <you@example.com>
2022-07-21 21:52:55 +0300

    "Чт 21 июл 2022 21:52:55" - поправил file.sh git.sh собираюсь создать сборку всех файлов в один

--- git.sh
+++ git.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # Git
 alias gst="git status"
 alias glog="git log"
@@ -7,26 +8,30 @@ alias gcd="git checkout $1"
 alias gmer="git merge $1"
 # Разница между коммитами или ветками
 alias gdif="git diff $1"
-alias gback="git reset --hard"
-
+alias grst="git reset --hard"
 
 gadd() {
-    date=`date +\"%c\"`
+    # Создать комит всех изменений
+    date=$(date +\"%c\")
     req="git add -A && git commit -m '$date - $1'"
     echo $req
     eval $req
 }
-
-gaddp(){
-    gadd $@;
-    echo 'git push';  
-    git push;
+gaddp() {
+    # Создать комит всех изменений и выполнить push
+    gadd $@
+    echo 'git push'
+    git push
 }
-
 garch() {
     # Сделать архив текущей ветки
     req="git archive --format zip --output $1.zip "
     req+=$(git rev-parse --abbrev-ref HEAD)
     echo $req
     eval $req
+}
+
+grmh(){
+    # Удалить файл из отслеживания
+	git rm --cached $1
 }
