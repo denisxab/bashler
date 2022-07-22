2c5b48db2029c18b369e763aa27b01308e59607a
Ваше Имя <you@example.com>
2022-07-22 20:57:10 +0300

    "Пт 22 июл 2022 20:57:10" - Алиасы должны вставляться в начло source.sh

--- _build.py
+++ _build.py
@@ -1,18 +1,15 @@
 
 """
-Собрать все .sh файл в один файл, это 
+Собрать все .sh файл в один файл, это
 нужно для удобства импортирования
 """
 import pathlib
-path =pathlib.Path(__file__).resolve().parent 
+path = pathlib.Path(__file__).resolve().parent
 print(path)
 source_code = (path / 'source_code.sh')
 source_code.write_text('')
-res='\n'.join([x.read_text() for x in (path/ 'sh').glob("*.sh")])
+res = '\n'.join([x.read_text() for x in (path / 'sh').glob("*.sh")])
 print(f"Ok {len(res)} symbol")
-source_code.write_text(res)
-
-
-
-
-
+# В начало файла вставляем алиасы, для успешного использования их в функциях
+res_alias = (path / 'alias.sh')
+source_code.write_text(res_alias.read_text() + res)
