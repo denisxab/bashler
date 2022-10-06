6fa1c05d45c00221df124d5137c26e97b2d3bb9c
Your Name <you@example.com>
2022-10-06 22:31:38 +0300

    "Чт 06 окт 2022 22:31:37" -

--- sh/python_pro.sh
+++ sh/python_pro.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
 
 # pep 8
-pepd() {
-    # Отформатировать все python файлы в указанной диреткории
-    # [путь_к_папке] = по умолчанию так где сейчас
-    dir=$1
-    if [[ -z $dir ]]; then
-        dir=$(pwd)
-    fi
-
-    ~py -m autopep8 --in-place --aggressive --aggressive -v -r $dir
+pep() {
+    # $1 =  путь к файлу или папки, если указана папка то тогда отформатируется вссе файлы в этой папке
+    pimport $1 && ppep8 $1
 }
 
-pepf() {
+ppep8() {
     # Отформатировать python файл
-    # [путь_к_файлу]
+    # $1 =  путь к файлу или папки, если указана папка то тогда отформатируется вссе файлы в этой папке
+    # Установить `pip install autopep8`
+    if [[ -f $1 ]]; then
+        ~py -m autopep8 --in-place --aggressive -v $1
+    elif [[ -d $1 ]]; then
+        ~py -m autopep8 --in-place --aggressive -v -r $1
+    fi
 
-    ~py -m autopep8 --in-place --aggressive --aggressive -v $1
+}
+
+pimport() {
+    # Форматировать иморты, удалить не используемые импорты
+    # $1 =  путь к файлу или папки, если указана папка то тогда отформатируется вссе файлы в этой папке
+    # Установить `pip install autoflake`
+    if [[ -f $1 ]]; then
+        ~py -m autoflake --in-place --remove-unused-variables $1
+    elif [[ -d $1 ]]; then
+        ~py -m autoflake --in-place --remove-unused-variables -r $1
+    fi
 }
 
 # PIP
@@ -30,6 +40,7 @@ pvenv() {
     ~py -m venv venv
 }
 
+# Poetry
 poetry_init() {
     # Звгрузить poetry
     pip install cachecontrol
