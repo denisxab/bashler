8b62f1b87012bb7b0a54213229ecd54d21818294
Your Name <you@example.com>
2022-11-06 13:25:48 +0300

    "Вс 06 ноя 2022 13:25:48" -

--- alias.sh
+++ alias.sh
@@ -14,6 +14,7 @@ alias -s {js,py,cs,ts,html}=code
 # Глобальные Alias
 alias -g configer="$DiskData/MyProject/PycharmProjects/configer/venv/bin/python3.10 $DiskData/MyProject/PycharmProjects/configer/configer/main.py"
 alias -g gitclones="$DiskData/MyProject/PycharmProjects/git_clons/venv/bin/python3.10 $DiskData/MyProject/PycharmProjects/git_clons/git_clons/main.py"
+alias -g pytots="$DiskData/MyProject/python_to_ts_type/venv/bin/python3.11 $DiskData/MyProject/python_to_ts_type/main.py"
 alias -g showlogsmal="/home/denis/PycharmProjects/showlofsmal/showlogsmal.bin"
 alias -g ~py=python3.11
 alias -g pip="~py -m pip"
