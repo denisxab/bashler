650704c22e90322663cc97ba8a230739e4d97767
Your Name <you@example.com>
2022-12-12 00:23:22 +0300

    "Пн 12 дек 2022 00:23:21" -

--- sh/video.sh
+++ sh/video.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# 
+#
 ## Рабата с видео и Gif
 #
 
 -gifzip() {
-	# Сжать Gif видео
-    e="gifsicle -i $1 -o out_$1 --optimize=3 --colors=256 --lossy=30"  
+    # Сжать Gif видео
+    e="gifsicle -i \"$1\" -o \"out_$1\" --optimize=3 --colors=256 --lossy=30"
     echo $e
     eval $e
 }
