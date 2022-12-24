b71942024eef8ddf69c7ac1a1c6cf4042e5f59f8
Your Name <you@example.com>
2022-12-24 20:11:18 +0300

    "Сб 24 дек 2022 20:11:18" -

--- source_code.sh
+++ source_code.sh
@@ -1165,3 +1165,26 @@ poetry_init() {
     pip install poetry
     poetry install
 }
+
+# -------------------
+
+phttpserver() {
+    # Запустить сервер п разадчи файлов
+    # $1 порт
+    ~py -c '''
+from http.server import HTTPServer, SimpleHTTPRequestHandler, test
+import sys
+
+class CORSRequestHandler (SimpleHTTPRequestHandler):
+    def end_headers(self):
+        # Для разрешения CROS.(Запросы от других url)
+        self.send_header("Access-Control-Allow-Origin", "*")
+        SimpleHTTPRequestHandler.end_headers(self)
+
+test(
+    CORSRequestHandler,
+    HTTPServer,
+    port=int(sys.argv[1]) if len(sys.argv) > 1 else 8000,
+)
+''' $1
+}
