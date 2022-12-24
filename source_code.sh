00fcc92b1c42260e2e15ad520f4acc30e2defb66
Your Name <you@example.com>
2022-12-24 22:01:24 +0300

    "Сб 24 дек 2022 22:01:24" -

--- source_code.sh
+++ source_code.sh
@@ -8,22 +8,22 @@ export DiskData="/media/denis/dd19b13d-bd85-46bb-8db9-5b8f6cf7a825"
 export BASHLER_PATH_PY="$BASHLER_PATH/py"
 ######################################################################################
 # Открывать файлы с указаным разширенем в указанной программе
-alias -s {txt,md,conf,log}=micro
-alias -s {js,py,cs,ts,html}=code
+alias {txt,md,conf,log}=micro
+alias {js,py,cs,ts,html}=code
 ######################################################################################
 # 
 # Глобальные Alias
 # 
 # Алиасы для программ
-alias -g configer="$DiskData/MyProject/PycharmProjects/configer/venv/bin/python3.10 $DiskData/MyProject/PycharmProjects/configer/configer/main.py"
-alias -g gitclones="$DiskData/MyProject/PycharmProjects/git_clons/venv/bin/python3.10 $DiskData/MyProject/PycharmProjects/git_clons/git_clons/main.py"
-alias -g pytots="$DiskData/MyProject/python_to_ts_type/venv/bin/python3.11 $DiskData/MyProject/python_to_ts_type/main.py"
-alias -g showlogsmal="/home/denis/PycharmProjects/showlofsmal/showlogsmal.bin"
-alias -g ~py=python3.11
-alias -g pip="~py -m pip"
-alias -g ~bpy="~py -m bpython"
-alias -g syncthing="$DiskData/AlienApp/aplication/other/Syncthing/syncthing-linux-amd64-v1.20.1/syncthing"
-alias -g dbeaver="snap run dbeaver-ce"
+alias configer="$DiskData/MyProject/PycharmProjects/configer/venv/bin/python3.10 $DiskData/MyProject/PycharmProjects/configer/configer/main.py"
+alias gitclones="$DiskData/MyProject/PycharmProjects/git_clons/venv/bin/python3.10 $DiskData/MyProject/PycharmProjects/git_clons/git_clons/main.py"
+alias pytots="$DiskData/MyProject/python_to_ts_type/venv/bin/python3.11 $DiskData/MyProject/python_to_ts_type/main.py"
+alias showlogsmal="/home/denis/PycharmProjects/showlofsmal/showlogsmal.bin"
+alias ~py=python3.11
+alias pip="~py -m pip"
+alias ~bpy="~py -m bpython"
+alias syncthing="$DiskData/AlienApp/aplication/other/Syncthing/syncthing-linux-amd64-v1.20.1/syncthing"
+alias dbeaver="snap run dbeaver-ce"
 # 
 alias ..="cd .."
 # замена ls на NNN
