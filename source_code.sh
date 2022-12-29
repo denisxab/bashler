f786d887ad3a444aa469bed384a35538ef5f1d4b
Your Name <you@example.com>
2022-12-29 22:18:18 +0300

    "Чт 29 дек 2022 22:18:18" -

--- source_code.sh
+++ source_code.sh
@@ -277,34 +277,6 @@ else:
 }
 #!/bin/bash
 
--rsync-local-folder() {
-	# Синхронизировать локальные папки
-	# > откуда куда
-	# -e папка_1 папка_... 	= Исключить папки или файлы из сихронизации
-	# --dry-run			 	= Показать какие файлы будут сихронезированы без выполени программы
-	exclud_folder=$(__rsync-exlude-folder $@)
-	res="rsync -azvh --progress $1 $2 $exclud_folder"
-	echo $res
-	eval $res
-}
--rsync-delete-local-folder() {
-	# Синхронизировать папки, если в ВЫХОДНОЙ(out) папке отличия, то удалить их
-	# -e папка_1 папка_... = Исключить папки или файлы из сихронизации
-	exclud_folder=$(__rsync-exlude-folder $@)
-	res="rsync -azvh --progress --delete $1 $2 $exclud_folder"
-	echo $res
-	eval $res
-}
--rsync-server-folder() {
-	# Синхронезировать с сервером по SSH
-	# > port username@ip:path localpath
-	# -e папка_1 папка_... = Исключить папки или файлы из сихронизации
-	exclud_folder=$(__rsync-exlude-folder $@)
-	SSH_RES="ssh -p $1"
-	res="rsync -azvh --progress -e $SSH_RES $2 $3 $exclud_folder"
-	echo $res
-	eval $res
-}
 -rsync-server() {
 	# Синхронезировать с сервером по SSH, если в ВЫХОДНОЙ(out) папке отличия, то удалить их
 	#
@@ -339,7 +311,7 @@ else:
 	fi
 	#
 	# Нужно ли удалять файлы и папки в `out` если их нет в `in`
-	# 
+	#
 	is_delete=""
 	if [[ ${_f[*]} =~ "d" ]]; then
 		is_delete='--delete'
@@ -350,50 +322,6 @@ else:
 	res="rsync -azvh --progress $is_delete ${_p[1]} ${_p[2]} $SSH_RES $exclud_folder"
 	echo $res
 	eval $res
-}
-
-##############
--rsync-read-file() {
-	# Прочитать файл с сохранеными путями синхронизации
-	eval $(~py -c "
-import os.path
-file = '.rsyncpath'
-if os.path.exists(file):
-    with open(file, 'r') as _f:
-        print(_f.read())
-	" $@)
-}
-__rsync-exlude-folder() {
-	# -e папка_1 папка_... = Исключить папки или файлы из сихронизации
-	# можно создать файл .rsyncignore(по типу .gitignore) для хранения исключений
-	~py -c "
-import os.path
-import sys
-
-def main(argv:list):
-    '''
-    >>> main(['sd', 'in', 'out', '-e', 'ewe', 'edde', 'deed'])
-    --exclude=ewe --exclude=edde --exclude=deed
-    '''
-    file = '.rsyncignore'
-    exclude = []
-    # Получаем исключения из консоли
-    exclude.extend(argv[3:])
-    if exclude and exclude.pop(0) == '-e':
-        # Получаем исключения из файла
-        if os.path.exists(file):
-            with open(file, 'r') as _f:
-                exclude.extend(_f.read().split('\n'))
-        res = ''
-        for x in exclude:
-            res += '--exclude=%s ' % x
-        # убрать последний пробел
-        res = res[:-1] if res.endswith(' ') else res
-        print(res)
-    else:
-        print('', end='')
-main(sys.argv)
-	" $@
 }
 
 -rsync-parse-conf() {
@@ -422,6 +350,80 @@ else:
 	echo $res
 	return 0 # Выйти из функции 0 хорошо 1 плохо
 }
+
+#################
+# -rsync-local-folder() {
+# 	# Синхронизировать локальные папки
+# 	# > откуда куда
+# 	# -e папка_1 папка_... 	= Исключить папки или файлы из сихронизации
+# 	# --dry-run			 	= Показать какие файлы будут сихронезированы без выполени программы
+# 	exclud_folder=$(__rsync-exlude-folder $@)
+# 	res="rsync -azvh --progress $1 $2 $exclud_folder"
+# 	echo $res
+# 	eval $res
+# }
+# -rsync-delete-local-folder() {
+# 	# Синхронизировать папки, если в ВЫХОДНОЙ(out) папке отличия, то удалить их
+# 	# -e папка_1 папка_... = Исключить папки или файлы из сихронизации
+# 	exclud_folder=$(__rsync-exlude-folder $@)
+# 	res="rsync -azvh --progress --delete $1 $2 $exclud_folder"
+# 	echo $res
+# 	eval $res
+# }
+# -rsync-server-folder() {
+# 	# Синхронезировать с сервером по SSH
+# 	# > port username@ip:path localpath
+# 	# -e папка_1 папка_... = Исключить папки или файлы из сихронизации
+# 	exclud_folder=$(__rsync-exlude-folder $@)
+# 	SSH_RES="ssh -p $1"
+# 	res="rsync -azvh --progress -e $SSH_RES $2 $3 $exclud_folder"
+# 	echo $res
+# 	eval $res
+# }
+
+##############
+# -rsync-read-file() {
+# 	# Прочитать файл с сохранеными путями синхронизации
+# 	eval $(~py -c "
+# import os.path
+# file = '.rsyncpath'
+# if os.path.exists(file):
+#     with open(file, 'r') as _f:
+#         print(_f.read())
+# 	" $@)
+# }
+# __rsync-exlude-folder() {
+# 	# -e папка_1 папка_... = Исключить папки или файлы из сихронизации
+# 	# можно создать файл .rsyncignore(по типу .gitignore) для хранения исключений
+# 	~py -c "
+# import os.path
+# import sys
+
+# def main(argv:list):
+#     '''
+#     >>> main(['sd', 'in', 'out', '-e', 'ewe', 'edde', 'deed'])
+#     --exclude=ewe --exclude=edde --exclude=deed
+#     '''
+#     file = '.rsyncignore'
+#     exclude = []
+#     # Получаем исключения из консоли
+#     exclude.extend(argv[3:])
+#     if exclude and exclude.pop(0) == '-e':
+#         # Получаем исключения из файла
+#         if os.path.exists(file):
+#             with open(file, 'r') as _f:
+#                 exclude.extend(_f.read().split('\n'))
+#         res = ''
+#         for x in exclude:
+#             res += '--exclude=%s ' % x
+#         # убрать последний пробел
+#         res = res[:-1] if res.endswith(' ') else res
+#         print(res)
+#     else:
+#         print('', end='')
+# main(sys.argv)
+# 	" $@
+# }
 
 #!/bin/bash
 
