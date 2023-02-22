eec08cabfedaf41880f7444b1592013a3f76ed66
Your Name <you@example.com>
2023-02-22 16:41:16 +0300

    "Ср 22 фев 2023 16:41:16" -

--- sh/ffmpeg.sh
+++ sh/ffmpeg.sh
@@ -3,7 +3,7 @@
 ffmpeg-video-to-audio() {
     # Извлечь аудио из видео
     # $1 = Путь к видео
-    res="ffmpeg -i $1 -vn -acodec copy $(basename $1 | cut -d. -f1).m4a"
+    res="ffmpeg -i '$1' -vn -acodec copy '$(basename $1 | cut -d. -f1).m4a'"
     echo $res
     eval $res
 }
--- source_code.sh
+++ source_code.sh
@@ -1384,7 +1384,7 @@ export Wireguard_VPN_CONF="wg0"
 ffmpeg-video-to-audio() {
     # Извлечь аудио из видео
     # $1 = Путь к видео
-    res="ffmpeg -i $1 -vn -acodec copy $(basename $1 | cut -d. -f1).m4a"
+    res="ffmpeg -i '$1' -vn -acodec copy '$(basename $1 | cut -d. -f1).m4a'"
     echo $res
     eval $res
 }
